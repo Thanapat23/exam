23999d3139c6879958682a3e63de1b77c5933c4a
benz <koop0408@gmail.com>
2022-02-26 11:51:39 +0700

    Format

--- src/register/Register.js
+++ src/register/Register.js
@@ -1,17 +1,17 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
-import {ip,port} from "../setIP/setting";
+import { ip, port } from "../setIP/setting";
 import { useHistory } from 'react-router-dom';
 
-class Register extends Component{
+class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            idkey:"",
-            firstname:"",
-            email:localStorage.getItem('email'),
-            lastname:"",
-            district:"",
+            idkey: "",
+            firstname: "",
+            email: localStorage.getItem('email'),
+            lastname: "",
+            district: "",
             data: []
         }
         this.handleChang = this.handleChang.bind(this);
@@ -24,26 +24,26 @@ class Register extends Component{
         console.log(e.target.id);
         console.log(e.target.value);
     }
-    handleClicked(){
+    handleClicked() {
         let url = `https://localhost:3000/data`;
         let data = {
-            idkey:this.state.idkey,
-            firstname:this.state.firstname,
-            lastname:this.state.lastname,
-            district:this.state.district,
-            email:this.state.email
+            idkey: this.state.idkey,
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            district: this.state.district,
+            email: this.state.email
         }
-        axios.post(url,data)
+        axios.post(url, data)
         this.setState({
-            idkey:"",
-            firstname:"",
-            lastname:"",
-            district:"",
-            email:""
+            idkey: "",
+            firstname: "",
+            lastname: "",
+            district: "",
+            email: ""
         });
         this.props.history.push('./Showdata');
     }
-    
+
     componentDidMount() {
         //console.log("before get data");
         this.getData();
@@ -53,37 +53,37 @@ class Register extends Component{
         console.log("before fetch data");
         fetch('/cmdistrict')
             .then(res => res.json())
-            .then(list => this.setState({ data:list }))
+            .then(list => this.setState({ data: list }))
         console.log("after fetch data");
     }
 
 
     render() {
-        return(
+        return (
             <div>
                 <div className="App">
-                <h2 className="my-4">Register<br/></h2>
-                    <hr/>
+                    <h2 className="my-4">Register<br /></h2>
+                    <hr />
                 </div>
                 <form className="container">
                     <div className="form-group">
-                        <label className="text-white"  htmlFor="id">ID</label>
-                        <input type="text" className="form-control" size="10" id="idkey" onChange={this.handleChang} value={this.state.idkey}/>
-                    </div>                   
+                        <label className="text-white" htmlFor="id">ID</label>
+                        <input type="text" className="form-control" size="10" id="idkey" onChange={this.handleChang} value={this.state.idkey} />
+                    </div>
                     <div className="form-group">
                         <label className="text-white" >First Name</label>
-                        <input type="text" className="form-control" id="firstname" onChange={this.handleChang} value={this.state.firstname}/>
+                        <input type="text" className="form-control" id="firstname" onChange={this.handleChang} value={this.state.firstname} />
                     </div>
                     <div className="form-group">
                         <label className="text-white"  >Last Name</label>
-                        <input type="text" className="form-control" id="lastname" onChange={this.handleChang} value={this.state.lastname}/>
+                        <input type="text" className="form-control" id="lastname" onChange={this.handleChang} value={this.state.lastname} />
                     </div>
                     <div>
                         <select className="form-group" id="district" value={this.state.district} onChange={this.handleChang} required>
                             <option value="">Select District</option>
-                                {this.state.data.map(item => {
-                                    return <option value={item.id}>{item.district_name}</option>
-                                })}
+                            {this.state.data.map(user => {
+                                return <option value={user.id}>{user.district_name}</option>
+                            })}
                         </select>
                     </div>
                     <button type="button" className="btn btn-primary" onClick={this.handleClicked}>Submit</button>
@@ -95,5 +95,5 @@ class Register extends Component{
 
 export default function WithRouter(props) {
     const history = useHistory();
-    return (<Register {...props} history={history}/>);
-  }
+    return (<Register {...props} history={history} />);
+}
